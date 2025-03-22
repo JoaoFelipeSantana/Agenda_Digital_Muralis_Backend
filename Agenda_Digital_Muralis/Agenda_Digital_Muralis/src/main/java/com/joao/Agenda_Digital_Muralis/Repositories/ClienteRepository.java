1b8857fc13462fd21bc4c459716c8666bf15d055
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-03-22 12:03:49 -0300

    [feat] - Criando o Repository  de Cliente

---
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Repositories/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.joao.Agenda_Digital_Muralis.Repositories;
+
+import com.joao.Agenda_Digital_Muralis.Domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+}
