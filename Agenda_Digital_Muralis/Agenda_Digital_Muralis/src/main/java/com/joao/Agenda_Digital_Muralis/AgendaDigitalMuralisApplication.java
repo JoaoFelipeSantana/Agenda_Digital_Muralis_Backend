12cfc56253b3807f53f7cd06de04ca30a7d94044
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-03-22 11:14:13 -0300

    [feat] - Adicionando a estrutura inicial do projeto

---
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/AgendaDigitalMuralisApplication.java
@@ -0,0 +1,13 @@
+package com.joao.Agenda_Digital_Muralis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDigitalMuralisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDigitalMuralisApplication.class, args);
+	}
+
+}
---
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/test/java/com/joao/Agenda_Digital_Muralis/AgendaDigitalMuralisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.Agenda_Digital_Muralis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaDigitalMuralisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
