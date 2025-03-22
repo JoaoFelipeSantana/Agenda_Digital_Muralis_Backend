a202da65f274c84f90cda434b9d5b4ebea0b6cdd
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-03-22 20:59:05 -0300

    [feat] - Adicionando o Repository para "cliente_endereco"

---
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Repositories/ClienteEnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.joao.Agenda_Digital_Muralis.Repositories;
+
+import com.joao.Agenda_Digital_Muralis.Domain.ClienteEndereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteEnderecoRepository extends JpaRepository<ClienteEndereco, Integer> {
+}
