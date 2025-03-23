327d6e40c64203a038be8c84f39c7927cdbc914d
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-03-22 21:41:00 -0300

    [feat] - Adicionando um novo construtor para o POST

--- Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Domain/Cliente.java
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Domain/Cliente.java
@@ -1,13 +1,14 @@
 package com.joao.Agenda_Digital_Muralis.Domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.joao.Agenda_Digital_Muralis.DTO.RegisterClienteDTO;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Table(name="cliente")
@@ -19,10 +20,18 @@ import java.util.Date;
 
 public class Cliente {
 
-    @Id
-    private int id;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCliente;
     private String nome;
     private String cpf;
     private Date dt_nascimento;
+
+    public Cliente(RegisterClienteDTO data) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        this.nome = data.nome();
+        this.cpf = data.cpf();
+        this.dt_nascimento = format.parse(data.dt_nascimento());
+    }
 
 }
