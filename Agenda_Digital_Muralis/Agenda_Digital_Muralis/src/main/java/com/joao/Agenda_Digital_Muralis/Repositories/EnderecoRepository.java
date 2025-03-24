582e7197cd955daed0ba6bdf0d67944b11125d84
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-03-24 19:24:40 -0300

    [feat] - Adicionando o método "findByCep" no enderecoRepository

--- Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Repositories/EnderecoRepository.java
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Repositories/EnderecoRepository.java
@@ -3,5 +3,8 @@ package com.joao.Agenda_Digital_Muralis.Repositories;
 import com.joao.Agenda_Digital_Muralis.Domain.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EnderecoRepository extends JpaRepository <Endereco, Integer> {
+    Optional<Endereco> findByCep(String cep);
 }
