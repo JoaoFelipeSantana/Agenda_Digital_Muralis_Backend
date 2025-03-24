285ca5e88711259bd528d835be7bb97022a9f42a
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-03-24 18:03:56 -0300

    [feat] - Adicionando a busca por nome e cpf no clienteRepository

--- Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Repositories/ClienteRepository.java
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Repositories/ClienteRepository.java
@@ -4,4 +4,6 @@ import com.joao.Agenda_Digital_Muralis.Domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    Cliente findByNome(String nome);
+    Cliente findByCpf(String cpf);
 }
