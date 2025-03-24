a6d40ca5bd24d8fff3d00fdfa4fd560567abf55c
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-03-24 14:38:57 -0300

    [feat] - Transferindo o CRUD para "clienteService"

--- Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Controllers/ClienteController.java
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Controllers/ClienteController.java
@@ -2,7 +2,7 @@ package com.joao.Agenda_Digital_Muralis.Controllers;
 
 import com.joao.Agenda_Digital_Muralis.DTO.ClienteDTO;
 import com.joao.Agenda_Digital_Muralis.Domain.Cliente;
-import com.joao.Agenda_Digital_Muralis.Repositories.ClienteRepository;
+import com.joao.Agenda_Digital_Muralis.Service.ClienteService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,65 +18,34 @@ import java.util.Optional;
 public class ClienteController {
 
     @Autowired
-    ClienteRepository clienteRepository;
+    ClienteService clienteService;
 
     @PostMapping
-    public ResponseEntity createCliente(@RequestBody ClienteDTO dados) throws ParseException {
-        Cliente newCliente = new Cliente(dados);
-        clienteRepository.save(newCliente);
-        return ResponseEntity.ok(dados);
+    @Transactional
+    public ResponseEntity<Cliente> createCliente(@RequestBody ClienteDTO dados) throws ParseException {
+        return ResponseEntity.ok(clienteService.registrarCliente(dados));
     }
 
     @GetMapping
-    public ResponseEntity getAllClientes() {
-        var allClientes = clienteRepository.findAll();
-        return ResponseEntity.ok(allClientes);
+    public ResponseEntity<List<Cliente>> getAllClientes() {
+        return ResponseEntity.ok(clienteService.listarTodos());
     }
 
     @GetMapping("/{id}")
-    @Transactional
-    public ResponseEntity getONECliente(@PathVariable int id) {
-        Optional<Cliente> optionalCliente = clienteRepository.findById(id);
-
-        if (optionalCliente.isPresent()) {
-            Cliente cliente = optionalCliente.get();
-
-            return ResponseEntity.ok(cliente);
-        }
-        return ResponseEntity.badRequest().body("Cliente não encontrado");
+    public ResponseEntity<Cliente> getONECliente(@PathVariable int id) {
+        return ResponseEntity.ok(clienteService.buscarPorId(id));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
     public ResponseEntity deleteCliente(@PathVariable int id) {
-        Optional<Cliente> optionalCliente = clienteRepository.findById(id);
-
-        if (optionalCliente.isPresent()) {
-            Cliente cliente = optionalCliente.get();
-            clienteRepository.delete(cliente);
-
-            return  ResponseEntity.noContent().build();
-        }
-        return  ResponseEntity.badRequest().body("Cliente não encontrado");
+        clienteService.deletar(id);
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity updateCliente(@PathVariable int id, @RequestBody ClienteDTO dados) throws ParseException {
-        Optional<Cliente> optionalCliente = clienteRepository.findById(id);
-
-        if (optionalCliente.isPresent()) {
-            Cliente cliente = optionalCliente.get();
-
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-
-            cliente.setCpf(dados.cpf());
-            cliente.setNome(dados.nome());
-            cliente.setDt_nascimento(format.parse(dados.dt_nascimento()));
-
-            clienteRepository.save(cliente);
-            return ResponseEntity.ok(cliente);
-        }
-        return  ResponseEntity.badRequest().body("Cliente não encontrado");
+    public ResponseEntity<Cliente> updateCliente(@PathVariable int id, @RequestBody ClienteDTO dados) throws ParseException {
+        return  ResponseEntity.ok(clienteService.atualizarCliente(id, dados));
     }
 }
