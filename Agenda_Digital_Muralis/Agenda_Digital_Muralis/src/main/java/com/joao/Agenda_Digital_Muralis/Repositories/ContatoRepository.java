4076e8c212fa29033c0473a73cfc2addb6d39d98
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-03-24 16:04:10 -0300

    [feat] - Adicionando o método de findByCliente ao Repository de "contato"

--- Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Repositories/ContatoRepository.java
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Repositories/ContatoRepository.java
@@ -1,7 +1,11 @@
 package com.joao.Agenda_Digital_Muralis.Repositories;
 
+import com.joao.Agenda_Digital_Muralis.Domain.Cliente;
 import com.joao.Agenda_Digital_Muralis.Domain.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContatoRepository extends JpaRepository <Contato, Integer> {
+    List<Contato> findByCliente(Cliente cliente);
 }
