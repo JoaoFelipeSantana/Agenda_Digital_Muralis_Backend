031cb8f8f89990e4fb2713dfc9448c5cbf8ec4a4
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-03-24 14:13:22 -0300

    [feat] - Adicionando o método PUT para clientes

--- Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Controllers/ClienteController.java
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Controllers/ClienteController.java
@@ -1,6 +1,6 @@
 package com.joao.Agenda_Digital_Muralis.Controllers;
 
-import com.joao.Agenda_Digital_Muralis.DTO.RegisterClienteDTO;
+import com.joao.Agenda_Digital_Muralis.DTO.ClienteDTO;
 import com.joao.Agenda_Digital_Muralis.Domain.Cliente;
 import com.joao.Agenda_Digital_Muralis.Repositories.ClienteRepository;
 import jakarta.transaction.Transactional;
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Optional;
 
 @RestController
@@ -19,7 +20,7 @@ public class ClienteController {
     ClienteRepository clienteRepository;
 
     @PostMapping
-    public ResponseEntity createCliente(@RequestBody RegisterClienteDTO dados) throws ParseException {
+    public ResponseEntity createCliente(@RequestBody ClienteDTO dados) throws ParseException {
         Cliente newCliente = new Cliente(dados);
         clienteRepository.save(newCliente);
         return ResponseEntity.ok(dados);
@@ -54,6 +55,26 @@ public class ClienteController {
             clienteRepository.delete(cliente);
 
             return  ResponseEntity.noContent().build();
+        }
+        return  ResponseEntity.badRequest().body("Cliente não encontrado");
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity updateCliente(@PathVariable int id, @RequestBody ClienteDTO dados) throws ParseException {
+        Optional<Cliente> optionalCliente = clienteRepository.findById(id);
+
+        if (optionalCliente.isPresent()) {
+            Cliente cliente = optionalCliente.get();
+
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+            cliente.setCpf(dados.cpf());
+            cliente.setNome(dados.nome());
+            cliente.setDt_nascimento(format.parse(dados.dt_nascimento()));
+
+            clienteRepository.save(cliente);
+            return ResponseEntity.ok(cliente);
         }
         return  ResponseEntity.badRequest().body("Cliente não encontrado");
     }
