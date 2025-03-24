acecfe98a27e8749393a4e0a777b7d79df0777e5
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-03-24 13:33:25 -0300

    [feat] - Adicionando o método DELETE ao "cliente"

--- Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Controllers/ClienteController.java
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Controllers/ClienteController.java
@@ -3,11 +3,13 @@ package com.joao.Agenda_Digital_Muralis.Controllers;
 import com.joao.Agenda_Digital_Muralis.DTO.RegisterClienteDTO;
 import com.joao.Agenda_Digital_Muralis.Domain.Cliente;
 import com.joao.Agenda_Digital_Muralis.Repositories.ClienteRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cliente")
@@ -27,5 +29,20 @@ public class ClienteController {
     public ResponseEntity getAllClientes() {
         var allClientes = clienteRepository.findAll();
         return ResponseEntity.ok(allClientes);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteCliente(@PathVariable int id) {
+        Optional<Cliente> optionalCliente = clienteRepository.findById(id);
+
+        if (optionalCliente.isPresent()) {
+            Cliente cliente = optionalCliente.get();
+            clienteRepository.delete(cliente);
+
+            return  ResponseEntity.noContent().build();
+        }
+        System.out.println("Cliente não encontrado");
+        return  ResponseEntity.badRequest().body("Cliente não encontrado");
     }
 }
