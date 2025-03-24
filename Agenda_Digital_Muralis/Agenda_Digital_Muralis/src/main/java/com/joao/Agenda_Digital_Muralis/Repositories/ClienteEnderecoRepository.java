2e0b5226d5bef3af67401ac695e618d183b2a1a3
João Felipe Santana de Freitas <j155c137@gmail.com>
2025-03-24 20:45:34 -0300

    [feat] - Adicionando o método findByCliente para ClienteEnderecoRepository

--- Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Repositories/ClienteEnderecoRepository.java
+++ Agenda_Digital_Muralis/Agenda_Digital_Muralis/src/main/java/com/joao/Agenda_Digital_Muralis/Repositories/ClienteEnderecoRepository.java
@@ -1,7 +1,11 @@
 package com.joao.Agenda_Digital_Muralis.Repositories;
 
+import com.joao.Agenda_Digital_Muralis.Domain.Cliente;
 import com.joao.Agenda_Digital_Muralis.Domain.ClienteEndereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClienteEnderecoRepository extends JpaRepository<ClienteEndereco, Integer> {
+    Optional<ClienteEndereco> findByCliente(Cliente cliente);
 }
